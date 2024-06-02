0cd2b8afad0fcd2cd46b1aa8a41a6c69ccdb0122
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-06-02 16:05:08 +0200

    Configure Vite and Netlify for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import {defineConfig} from 'vite'
-
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: []	
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        agileboard: resolve(__dirname, 'portfolio-item-agileboard/index.html'),
+        bookconnect: resolve(__dirname, 'portfolio-item-bookconnect/index.html'),
+        cachebank: resolve(__dirname, 'portfolio-item-cachebank/index.html'),
+      },
+    },
+  },
+});

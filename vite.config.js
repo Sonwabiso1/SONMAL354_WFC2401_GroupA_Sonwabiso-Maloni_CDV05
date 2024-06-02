347968d9448e3cf99dda480ed5d65a59b392c407
Sonwabiso <sonwabisolebokgang@gmail.com>
2024-06-02 16:15:41 +0200

    ensuring connection to path

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        agileboard: resolve(__dirname, 'portfolio-item-agileboard/index.html'),
-        bookconnect: resolve(__dirname, 'portfolio-item-bookconnect/index.html'),
-        cachebank: resolve(__dirname, 'portfolio-item-cachebank/index.html'),
+        agileboard: resolve(__dirname, './portfolio-item-agileboard/index.html'),
+        bookconnect: resolve(__dirname, './portfolio-item-bookconnect/index.html'),
+        cachebank: resolve(__dirname, './portfolio-item-cachebank/index.html'),
       },
     },
   },
